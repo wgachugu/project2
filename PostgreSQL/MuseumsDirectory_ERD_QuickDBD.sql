c91aedbe6efa8809f2cb5abf240a14e567653ef8
dave1407 <dmostacero@zalkinamericas.local>
2022-12-08 19:35:04 -0500

    PostgreSQL Update ERD

--- PostgreSQL/MuseumsDirectory_ERD_QuickDBD.sql
+++ PostgreSQL/MuseumsDirectory_ERD_QuickDBD.sql
@@ -6,76 +6,43 @@
 -- two dots ('..' - without quotes).
 
 CREATE TABLE "museums_location" (
-    "LocationID" int   NOT NULL,
-    "Museum_name" varchar   NULL,
-    "Legal_name" varchar   NOT NULL,
-    "Museum_type" varchar   NULL,
-    "Street_address" varchar   NULL,
-    "City" varchar   NULL,
-    "State" varchar   NULL,
-    "Zip_code" int   NULL,
+    "index"  SERIAL  NOT NULL,
+    "museum_name" text   NULL,
+    "legal_name" text   NULL,
+    "museum_type" text   NULL,
+    "street_address" text   NULL,
+    "city" text   NULL,
+    "state" text   NULL,
+    "zip_code" text   NULL,
     CONSTRAINT "pk_museums_location" PRIMARY KEY (
-        "LocationID"
-     ),
-    CONSTRAINT "uc_museums_location_Legal_name" UNIQUE (
-        "Legal_name"
-    )
+        "index"
+     )
 );
 
 -- Museums Fiscal Year 2015
 CREATE TABLE "museums_fy2015" (
-    "FY2015ID" int   NOT NULL,
-    "Common_name" varchar   NULL,
-    "Legal_name" varchar   NOT NULL,
-    "Phone" int   NULL,
-    "Weburl" varchar   NULL,
-    "Revenue_2015" money   NULL,
-    "Long" float   NULL,
-    "Lat" float   NULL,
+    "index"  SERIAL  NOT NULL,
+    "common_name" text   NULL,
+    "legal_name" text   NULL,
+    "phone" text   NULL,
+    "weburl" text   NULL,
+    "revenue_2015" money   NULL,
+    "long" float   NULL,
+    "lat" float   NULL,
     CONSTRAINT "pk_museums_fy2015" PRIMARY KEY (
-        "FY2015ID"
-     ),
-    CONSTRAINT "uc_museums_fy2015_Legal_name" UNIQUE (
-        "Legal_name"
-    )
+        "index"
+     )
 );
 
 -- Museums Fiscal Year 2013
 CREATE TABLE "museums_fy2013" (
-    "FY2013ID" int   NOT NULL,
-    "Museum_name" varchar   NULL,
-    "Legal_name" varchar   NOT NULL,
-    "Revenue_2013" money   NULL,
-    "Region_code" int   NULL,
+    "index"  SERIAL  NOT NULL,
+    "museum_name" text   NULL,
+    "legal_name" text   NULL,
+    "revenue_2013" money   NULL,
+    "region_code" int   NULL,
     CONSTRAINT "pk_museums_fy2013" PRIMARY KEY (
-        "FY2013ID"
-     ),
-    CONSTRAINT "uc_museums_fy2013_Legal_name" UNIQUE (
-        "Legal_name"
-    )
-);
-
--- Museums Compiled
-CREATE TABLE "museums_directory" (
-    "DirectoryID" int   NOT NULL,
-    "Legal_name" varchar   NOT NULL,
-    "Museum_type" varchar   NULL,
-    "Street_address" varchar   NULL,
-    "City" varchar   NULL,
-    "State" varchar   NULL,
-    "Zip_code" int   NULL,
-    "Lat" float   NULL,
-    "Long" float   NULL,
-    "Region_code" int   NULL,
-    "Phone" bigint   NULL,
-    "Weburl" varchar   NULL,
-    "Revenue_2013" money   NULL,
-    "Revenue_2015" money   NULL,
-    CONSTRAINT "pk_museums_directory" PRIMARY KEY (
-        "DirectoryID"
-     ),
-    CONSTRAINT "uc_museums_directory_Legal_name" UNIQUE (
-        "Legal_name"
-    )
+        "index"
+     )
 );
 
